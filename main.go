ba01678ff32cd45e81a27ed18a2a50b5b80efb61
Bayan2019 <saparbayevabt@gmail.com>
2023-12-09 20:41:49 -0800

    JSON

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -28,10 +29,16 @@ func main() {
 	api_router := chi.NewRouter()
 
 	api_router.Get("/healthz", handlerReadiness)
-	api_router.Get("/metrics", apiCfg.handlerMetrics)
 	api_router.Get("/reset", apiCfg.handlerReset)
+	api_router.Post("/validate_chirp", handlerChirpsValidate)
 
 	app_router.Mount("/api", api_router)
+
+	admin_router := chi.NewRouter()
+
+	admin_router.Get("/metrics", apiCfg.handlerMetrics)
+
+	app_router.Mount("/admin", admin_router)
 
 	corsMux := middlewareCors(app_router)
 
@@ -42,4 +49,56 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, returnVals{Valid: true})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
 }
